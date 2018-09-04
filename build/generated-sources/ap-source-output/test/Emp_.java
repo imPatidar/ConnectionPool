b5b65182919310138307f0d1f232bd4756b539f3
Pankaj M. Patidar <39170113+imPatidar@users.noreply.github.com>
2018-09-04 09:59:20 +0530

    Add files via upload

---
+++ build/generated-sources/ap-source-output/test/Emp_.java
@@ -0,0 +1,15 @@
+package test;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-01T16:07:52")
+@StaticMetamodel(Emp.class)
+public class Emp_ { 
+
+    public static volatile SingularAttribute<Emp, Integer> empid;
+    public static volatile SingularAttribute<Emp, String> empname;
+    public static volatile SingularAttribute<Emp, Double> salary;
+
+}
